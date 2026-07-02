fb5ab779fe68766d40dfcd38e3ff9bd6cd6155ff
Bhavyan Daiya <daiyabhavyan@gmail.com>
2026-07-02 13:34:55 +0530

    leetcode 88: merge 2 sorted arrays

--- Arrays/3_merge_2_sorted_arrays.py
+++ Arrays/3_merge_2_sorted_arrays.py
@@ -1,37 +1,63 @@
-nums1 = [1, 1, 1, 2, 4, 6, 7]
-nums2 = [1, 1, 2, 4, 5, 7, 8, 9, 10]
+# nums1 = [1, 1, 1, 2, 4, 6, 7]
+# nums2 = [1, 1, 2, 4, 5, 7, 8, 9, 10]
 
-def func(nums1, nums2):
-    n = len(nums1)
-    m = len(nums2)
-    i = nums1[0]
-    j = nums2[0]
-    merged = []
-    while i < n and j < m:
-        if (nums1[i] <= nums2[j]):
-            if len(merged) == 0 or merged[-1] != nums1[i]:      # first if to check if merged is empty, second if if merged has 1 or more elemetns
-                merged.append(nums1[i])
-            i+=1 
-        else:
-            if len(merged) == 0 or merged[-1] != nums2[j]:
-                merged.append(nums2[j])
-            j+=1
+# def func(nums1, nums2):
+#     n = len(nums1)
+#     m = len(nums2)
+#     i = nums1[0]
+#     j = nums2[0]
+#     merged = []
+#     while i < n and j < m:
+#         if (nums1[i] <= nums2[j]):
+#             if len(merged) == 0 or merged[-1] != nums1[i]:      # first if to check if merged is empty, second if if merged has 1 or more elemetns
+#                 merged.append(nums1[i])
+#             i+=1 
+#         else:
+#             if len(merged) == 0 or merged[-1] != nums2[j]:
+#                 merged.append(nums2[j])
+#             j+=1
     
-    # now if either i or j exhausts, we want to simply check merged and append
-    while i < n:
-        if len(merged) == 0 or merged[-1] != nums1[i]:      # first if to check if merged is empty, second if if merged has 1 or more elemetns
-            merged.append(nums1[i])
-        i+=1
+#     # now if either i or j exhausts, we want to simply check merged and append
+#     while i < n:
+#         if len(merged) == 0 or merged[-1] != nums1[i]:      # first if to check if merged is empty, second if if merged has 1 or more elemetns
+#             merged.append(nums1[i])
+#         i+=1
 
-    while j < m:
-        if len(merged) == 0 or merged[-1] != nums2[j]:
-            merged.append(nums2[j])
-        j+=1        
+#     while j < m:
+#         if len(merged) == 0 or merged[-1] != nums2[j]:
+#             merged.append(nums2[j])
+#         j+=1        
 
-    return merged
+#     return merged
 
-print(func(nums1, nums2))
+# print(func(nums1, nums2))
 
 
-# TC: O(n+m)    -> len of both arrays
-# SC: O(1)      -> nothing being stored
+# # TC: O(n+m)    -> len of both arrays
+# # SC: O(1)      -> nothing being stored
+
+
+# leetcode 88: Merge sorted arrays
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+
+def func(nums1, m, nums2, n):
+    last = m + n - 1
+
+    while m > 0 and n > 0:
+        if nums1[m-1] >= nums2[n-1]:
+            nums1[last] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[last] = nums2[n-1]
+            n -= 1
+        last -=1
+    
+    while n > 0:
+        nums1[last] = nums2[n-1]
+        n -= 1
+        last -= 1
+
+    return nums1
+
+print(func(nums1, 3, nums2, 3))
